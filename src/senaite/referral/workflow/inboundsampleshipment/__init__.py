b053c24b14a052feb9e2cc1abcd24e3b109fdb1b
Jordi Puiggené <jp@naralabs.com>
2022-03-04 15:04:00 +0100

    Typos

--- src/senaite/referral/workflow/inboundsampleshipment/__init__.py
+++ src/senaite/referral/workflow/inboundsampleshipment/__init__.py
@@ -4,8 +4,8 @@ from senaite.referral.workflow import TransitionEventHandler
 from senaite.referral.workflow.inboundsampleshipment import events
 
 
-def AfterTransitionEventHandler(sample, event): # noqa lowercase
+def AfterTransitionEventHandler(shipment, event): # noqa lowercase
     """Actions to be done just after a transition for an Inbound Sample
     shipment takes place
     """
-    TransitionEventHandler("after", sample, events, event)
+    TransitionEventHandler("after", shipment, events, event)
--- src/senaite/referral/workflow/outboundsampleshipment/__init__.py
+++ src/senaite/referral/workflow/outboundsampleshipment/__init__.py
@@ -4,8 +4,8 @@ from senaite.referral.workflow import TransitionEventHandler
 from senaite.referral.workflow.outboundsampleshipment import events
 
 
-def AfterTransitionEventHandler(sample, event): # noqa lowercase
+def AfterTransitionEventHandler(shipment, event): # noqa lowercase
     """Actions to be done just after a transition for an Outbound Sample
     Shipment takes place
     """
-    TransitionEventHandler("after", sample, events, event)
+    TransitionEventHandler("after", shipment, events, event)
