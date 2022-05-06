2e101dc2140fb6a075c53f30b0d23a4bc04ec7ab
Jordi Puiggené <jp@naralabs.com>
2022-05-06 19:02:51 +0200

    Reject samples when the inbound shipment is rejected

--- src/senaite/referral/workflow/inboundsampleshipment/events.py
+++ src/senaite/referral/workflow/inboundsampleshipment/events.py
@@ -22,3 +22,17 @@ def after_receive_inbound_shipment(shipment):
 
     # Mark the outbound shipment counterpart as delivered
     remote_lab.do_action(shipment, "deliver_outbound_shipment")
+
+
+def after_reject_inbound_shipment(shipment):
+    """Event fired after transition "reject_inbound_shipment" for an Inbound
+    Sample Shipment is triggered. All Samples from the shipment, even if not
+    yet received, are rejected too
+    """
+    # Reject all InboundSample objects (not-yet-received)
+    for inbound_sample in shipment.getInboundSamples():
+        do_action_for(inbound_sample, "reject_inbound_sample")
+
+    # Reject all AnalysisRequest objects (received samples)
+    for sample in shipment.getSamples():
+        do_action_for(sample, "reject")
