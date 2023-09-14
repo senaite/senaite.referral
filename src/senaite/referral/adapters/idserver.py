9a53a5d74fa177eba911028bedfec7845f0e611c
Jordi Puiggené <jp@naralabs.com>
2023-09-14 13:21:38 +0200

    Redux of IIDserver's get_variables

--- src/senaite/referral/adapters/idserver.py
+++ src/senaite/referral/adapters/idserver.py
@@ -18,18 +18,9 @@
 # Copyright 2021-2022 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
-from senaite.referral.interfaces import IInboundSampleShipment
-from senaite.referral.interfaces import IOutboundSampleShipment
-from zope.interface import implementer
-
 from bika.lims import api
-from bika.lims.idserver import get_type_id
 from bika.lims.interfaces import IIdServerVariables
-
-SHIPMENT_TYPES = [
-    "InboundSampleShipment",
-    "OutboundSampleShipment",
-]
+from zope.interface import implementer
 
 
 @implementer(IIdServerVariables)
@@ -44,28 +35,8 @@ class IDServerVariablesAdapter(object):
         """Returns additional variables for ID Server depending on the type of
         the current context
         """
-        variables = {}
-        portal_type = get_type_id(self.context, **kw)
-
-        # get laboratory the shipment belongs to
         lab = kw.get("container") or api.get_parent(self.context)
-
-        if portal_type in SHIPMENT_TYPES:
-            variables.update({
-                "lab_code": lab.getCode()
-            })
-
+        variables = {
+            "lab_code": lab.getCode(),
+        }
         return variables
-
-    def get_lab_code(self, shipment):
-        """Returns the code of the laboratory the shipment is assigned to
-        """
-        shipment = api.get_object(shipment)
-        if IOutboundSampleShipment.providedBy(shipment):
-            laboratory = shipment.getReferenceLaboratory()
-        elif IInboundSampleShipment.providedBy(shipment):
-            laboratory = shipment.getReferringLaboratory()
-        else:
-            str_type = repr(type(shipment))
-            return ValueError("Type not supported: {}".format(str_type))
-        return laboratory.getCode()
